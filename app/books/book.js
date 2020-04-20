999b5e2bf14aaba1888e17293af2786536ac211e
unknown <ahmed.m.mansour1111@gmail.com>
2020-04-20 21:58:09 +0200

    init

---
+++ app/books/book.js
@@ -0,0 +1,15 @@
+"use strict";
+// export interface IBook {
+//   bookTitle: string;
+//   bookAuthor: string;
+//   productCode?: string;
+//   publishedOn: Date;
+//   bookDescription: string;
+//   genre?: string;
+//   specifications?: string;
+//   inStock: string;
+//   bookPrice: number;
+//   bookReviews: number;
+//   bookImageUrl: string;
+// }
+//# sourceMappingURL=book.js.map
